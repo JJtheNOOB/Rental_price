9c54d9b1cf00c81bef25b3c971f1037ef8f47b22
JJtheNOOB <41119352+JJtheNOOB@users.noreply.github.com>
2020-09-08 21:11:51 -0400

    Add files via upload

---
+++ Model Deployment/Streamlit Version/app.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import pandas as pd
+from geopy.geocoders import Nominatim
+import pickle
+
+#title
+st.title("Toronto Housing Price Evaluation")
+
+#add pictures
+image = "https://wp-tid.zillowstatic.com/trulia/wp-content/uploads/sites/3/2019/05/how-to-rent-out-your-house-e14146.png"
+st.image(image, caption='Rental Image',use_column_width=True)
+
+#input features
+features = []
+
+#load the model
+model = pickle.load(open('thresh_3000_model.pkl', 'rb'))
+
+#Get address and return a map
+address = st.text_input("1. Please enter your street address followed by Toronto (eg: 7100 Woodbine Ave, Toronto)")
+if address:
+    geolocator = Nominatim(user_agent="haha@hotmail.com")
+    location = geolocator.geocode(address)
+    if location and location.longitude < -78.6 and location.longitude > -79.8 and location.latitude > 43.1 and location.latitude < 44:
+        features.insert(0, location.longitude)
+        features.insert(0, location.latitude)
+        #Plot the location
+        if st.checkbox('Show Map'):
+            map_data = pd.DataFrame(
+                 [features],
+                columns=['lat', 'lon'])
+            st.write("Here is your approximate location:")
+            st.map(map_data)
+
+        num_bedroom = st.number_input("2. Please enter number of bedrooms", 1)
+        features.append(num_bedroom)
+        op_1 = st.selectbox('3. Air Conditioning Available?',('Yes', 'No'))
+        if op_1 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+        op_2 = st.selectbox('4. Are Water Bills Free?',('Yes', 'No'))
+        if op_2 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+        op_3 = st.selectbox('5. Are Storage Rooms / Lockers Available?',('Yes', 'No'))
+        if op_3 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+        op_4 = st.selectbox('6. Are Parking Spaces Available?',('Yes', 'No'))
+        if op_4 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+        op_5 = st.selectbox('7. Is the parking space underground?',('Yes', 'No'))
+        if op_5 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+        op_6 = st.selectbox('8. Is gym / exercise room Available?',('Yes', 'No'))
+        if op_6 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+        op_7 = st.selectbox('9. Are laundry rooms / facilities Available?',('Yes', 'No'))
+        if op_7 == "yes":
+            features.append(1)
+        else:
+            features.append(0)
+
+        df = pd.DataFrame([features], columns = ['latitude', 'longtitude', 'bedroom_num', 'AirConditioning',
+                                                    'Water', 'StorageLockers', 'Parking', 'UndergroundParking',
+                                                    'ExerciseRoom', 'LaundryFacilities'])
+
+        def run_result():
+            prediction = model.predict(df)[0]
+
+            if prediction <= 1600 or prediction >= 3000:
+                message = "We are not so confident on our price evaluation in this case, please try evaluate rental price within range 1600 to 3000 CAD"
+            elif (prediction > 1600 and prediction <= 2100) or (prediction >= 2100 and prediction < 3000):
+                message = "We are very confidence that about our price evaluation within range {} CAD to {} CAD".format(
+                    round((prediction - 150), 2), round((prediction + 150)), 2)
+            else:
+                message = "There is something wrong in the address input, please retry"
+
+            st.write("------------------------------------------")
+
+            st.title("**{}**".format(message))
+
+
+        if st.button("Click here to calculate"):
+            run_result()
+    elif not location:
+        st.write("Wrong Format of Address Entered! Please try modify the address.")
+    else:
+        st.write("Please enter an address within Greater Toronto Area.")
+
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("style.css")
+
+
+st.write("Developed by JJ Lian")
+
+
+
+
+
